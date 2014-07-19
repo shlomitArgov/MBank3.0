eed083d3cb76d7a8113081efbe8176f21e8a9c00
shlomitArgov <Shlomit@Dave>
2014-07-19 13:33:42 +0300

    added some javadoc comments for table creation class

--- CreateDB/src/tableOperations/CreateTables.java
+++ CreateDB/src/tableOperations/CreateTables.java
@@ -11,9 +11,22 @@ import tableOperations.dbStructure.DBVarTypes;
 
 /**
  * @author Shlomit Argov
- *
  */
 public class CreateTables {
+	/**
+	 * This class provides a service method (createTable) that creates tables in a given 
+	 * DB. It currently supports primary keys that are auto incremental integer
+	 * values or no primary key for a table.
+	 * 
+	 * @param con - Connection to DB
+	 * @param tableName - Table name in DB
+	 * @param primaryKeyName - name of primary key auto incremental numeric column. 
+	 * If null - no primamry key column is created. 
+	 * @param columnNames - Names of columns apart for the primary key column
+	 * @param columnTypes - Types of all columns apart for the primary key column
+	 * @return
+	 * @throws SQLException
+	 */
 	public static boolean createTable(Connection con, String tableName, String primaryKeyName, String[]  columnNames, DBVarTypes[] columnTypes) throws SQLException
 	{
 		String sql = "CREATE TABLE " + tableName + " (";
@@ -40,12 +53,3 @@ public class CreateTables {
 		return true;
 	}
 }
-
-/*
-CREATE TABLE students
-(
-id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-name VARCHAR(24) NOT NULL,
-address VARCHAR(1024),
-CONSTRAINT primary_key PRIMARY KEY (id)
-) ;*/
