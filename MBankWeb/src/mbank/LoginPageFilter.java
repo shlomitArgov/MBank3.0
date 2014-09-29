e9b83a4a79277203f0abce55d6150ba818c72d0e
shlomitArgov <shlomit.argov@gmail.com>
2014-09-29 18:46:36 +0300

    Added LoginFilter - redirects to account.jsp from the index.jsp page if a session already exists

---
+++ MBankWeb/src/mbank/LoginPageFilter.java
@@ -0,0 +1,68 @@
+package mbank;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoginPageFilter
+ */
+@WebFilter("/index.jsp")
+public class LoginPageFilter implements Filter 
+{
+    private static final String ACCOUNT_JSP = "/account.jsp";
+	private static final String APPLICATION_NAME = "/MBankWeb"; 
+
+	/**
+     * Default constructor. 
+     */
+    public LoginPageFilter() 
+    {
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() 
+	{
+	
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
+	{
+		System.out.println("LoginPageFilter.doFilter()");
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession(false);
+
+		// If a valid session already exists and reached this filter (url is index.jsp)
+		if(session != null)
+		{
+			System.out.println("Valid session exists - redirecting to " + APPLICATION_NAME + ACCOUNT_JSP);
+			((HttpServletResponse) response).sendRedirect(APPLICATION_NAME + ACCOUNT_JSP);
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
