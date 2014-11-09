aa8b1b9df3b56100b642760e1b9e71693f242fab
shlomitArgov <shlomit.argov@gmail.com>
2014-11-09 10:22:23 +0200

    Cleaned up controller code, added comments.

--- MBankWeb/src/mbank/Controller.java
+++ MBankWeb/src/mbank/Controller.java
@@ -19,19 +19,16 @@ import mbank.database.beans.Deposit;
 import mbank.database.beans.Property;
 import mbank.database.beans.enums.SystemProperties;
 import mbank.exceptions.MBankException;
+
 /**
  * Servlet implementation class Controller
  */
 @WebServlet(value = "/Controller", loadOnStartup = 1)
-
-public class Controller extends HttpServlet 
+public class Controller extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final String COMMAND_PARAM = "command";
-	private static final String USERNAME_PARAM = "username";
-	private static final String PASSWORD_PARAM = "password";
-	
 	private static final String LOGIN_COMMAND_PARAM = "Login";
 	private static final String MY_DETAILS_COMMAND_PARAM = "myDetails";
 	private static final String DEPOSITS_COMMAND_PARAM = "deposits";
@@ -42,7 +39,9 @@ public class Controller extends HttpServlet
 	private static final String MBANK_PROPERTIES_COMMAND_PARAM = "mbank_properties";
 	private static final String LOGOUT_COMMAND_PARAM = "logout";
 
-	
+	private static final String USERNAME_PARAM = "username";
+	private static final String PASSWORD_PARAM = "password";
+
 	private static final String MY_DETAILS_JSP = "/my_details.jsp";
 	private static final String DEPOSITS_JSP = "/deposits.jsp";
 	private static final String RECENT_ACTIVITIES_JSP = "/recent_activities.jsp";
@@ -67,125 +66,128 @@ public class Controller extends HttpServlet
 	private static final String DEPOSIT_ERROR_ATTR = "deposit_error";
 	private static final String SYSTEM_PROPERTIES_ATTR = "system_properties";
 
-	public void init(ServletConfig config) throws ServletException {
+	public void init(ServletConfig config) throws ServletException
+	{
 		super.init(config);
-		//TODO move the mbank instance to the application instance
 		MBank mbankInstance = MBank.getInstance();
+		// Create an instance of MBank and save it in the application context
 		this.getServletContext().setAttribute(MBABK_INSTANCE_ATTR, mbankInstance);
 	}
-	
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-			System.out.println("Controller.service()");
-			// handle command
-			String nextPage = null;
-			String command = request.getParameter(COMMAND_PARAM);
-			System.out.println("command = " + command);
+		// At this point the filter method doFilter of the Request filter has
+		// already run so we can be assured that a valid session
+		// and LoginAction objects exist
 
-			if (command == null)
+		System.out.println("Controller.service()");
+		// Handle command
+		String nextPage = null;
+		String command = request.getParameter(COMMAND_PARAM);
+		// TODO remove trace message
+		System.out.println("command = " + command);
+
+		if (command == null)
+		{
+			nextPage = ACCOUNT_JSP; // Default landing page
+		} else
+		{
+			switch (command)
+			{
+				case LOGIN_COMMAND_PARAM:
 				{
-					nextPage = ACCOUNT_JSP; // default (there should always
-											// be a valid session at this
-											// point)
-				} else
-				{
-					switch (command)
-						{
-						case LOGIN_COMMAND_PARAM:
-							{
-								try
-								{
-									nextPage = login(request);
-								} catch (MBankException e)
-								{
-									// TODO Auto-generated catch block
-									e.printStackTrace();
-								}
-								System.out
-										.println("nextPage after login command is: "
-												+ nextPage);
-								break;
-							}
-						case ACCOUNT_COMMAND_PARAM:
-							{
-								try
-								{
-									nextPage = gotoAccount(request);
-								} catch (MBankException e)
-								{
-									// TODO Auto-generated catch block
-									
-								}
-								break;
-							}
-						case WITHDRAW_COMMAND_PARAM:
-						{
-							//TODO
-							try
-							{
-								nextPage = withdrawFromAccount(request);
-							} catch (MBankException e)
-							{
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							break;
-						}
-						case DEPOSIT_COMMAND_PARAM:
-						{
-							//TODO
-							try
-							{
-								nextPage = depositToAccount(request);
-							} catch (MBankException e)
-							{
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							break;
-						}
-						case RECENT_ACTIVITIES_COMMAND_PARAM:
-							{
-								nextPage = gotoRecentActivities(request);
-								break;
-							}
-						case DEPOSITS_COMMAND_PARAM:
-							{
-								//TODO
-								nextPage = gotoDeposits(request);
-								break;
-							}
-						case MY_DETAILS_COMMAND_PARAM:
-							{
-								try
-								{
-									nextPage = gotoMyDetails(request);
-								} catch (MBankException e)
-								{
-									// TODO Auto-generated catch block
-									e.printStackTrace();
-								}
-								break;
-							}
-						case MBANK_PROPERTIES_COMMAND_PARAM:
-							{
-								nextPage = gotoMBankProperties(request);
-								break;
-							}
-						case LOGOUT_COMMAND_PARAM:
-							{
-								nextPage = logout(request);
-								break;
-							}
-						default:
-							{
-								nextPage = ACCOUNT_JSP;
-								break;
-							}
-						}
+					try
+					{
+						nextPage = login(request);
+					} catch (MBankException e)
+					{
+						// TODO remove trace message
+						e.printStackTrace();
+					}
+					// TODO remove trace message
+					System.out.println("nextPage after login command is: " + nextPage);
+					break;
 				}
-// forward the request
-	this.getServletContext().getRequestDispatcher(nextPage).forward(request, response);
+				case ACCOUNT_COMMAND_PARAM:
+				{
+					try
+					{
+						nextPage = gotoAccount(request);
+					} catch (MBankException e)
+					{
+						// TODO remove trace message
+						e.printStackTrace();
+					}
+					break;
+				}
+				case WITHDRAW_COMMAND_PARAM:
+				{
+					// TODO implement
+					try
+					{
+						nextPage = withdrawFromAccount(request);
+					} catch (MBankException e)
+					{
+						// TODO remove trace message
+						e.printStackTrace();
+					}
+					break;
+				}
+				case DEPOSIT_COMMAND_PARAM:
+				{
+					// TODO implement
+					try
+					{
+						nextPage = depositToAccount(request);
+					} catch (MBankException e)
+					{
+						//TODO remove trace message
+						e.printStackTrace();
+					}
+					break;
+				}
+				case RECENT_ACTIVITIES_COMMAND_PARAM:
+				{
+					nextPage = gotoRecentActivities(request);
+					break;
+				}
+				case DEPOSITS_COMMAND_PARAM:
+				{
+					// TODO implement
+					nextPage = gotoDeposits(request);
+					break;
+				}
+				case MY_DETAILS_COMMAND_PARAM:
+				{
+					try
+					{
+						nextPage = gotoMyDetails(request);
+					} catch (MBankException e)
+					{
+						// TODO remove trace message
+						e.printStackTrace();
+					}
+					break;
+				}
+				case MBANK_PROPERTIES_COMMAND_PARAM:
+				{
+					nextPage = gotoMBankProperties(request);
+					break;
+				}
+				case LOGOUT_COMMAND_PARAM:
+				{
+					nextPage = logout(request);
+					break;
+				}
+				default:
+				{
+					nextPage = ACCOUNT_JSP;
+					break;
+				}
+			}
+		}
+		// forward the request
+		this.getServletContext().getRequestDispatcher(nextPage).forward(request, response);
 	}
 
 	private String depositToAccount(HttpServletRequest request) throws MBankException
@@ -196,13 +198,12 @@ public class Controller extends HttpServlet
 		try
 		{
 			depositAmount = validatePositiveDouble(inputAmount);
-		}
-		catch(MBankException e)
+		} catch (MBankException e)
 		{
 			String error = e.getLocalizedMessage();
 			request.setAttribute(DEPOSIT_ERROR_ATTR, error);
 		}
-		if(request.getAttribute(DEPOSIT_ERROR_ATTR) == null)
+		if (request.getAttribute(DEPOSIT_ERROR_ATTR) == null)
 		{
 			try
 			{
@@ -216,7 +217,7 @@ public class Controller extends HttpServlet
 		}
 		setCommissionRateInRequest(request, clientAction);
 
-		//refresh the account.jsp page (account balance has changed)
+		// refresh the account.jsp page (account balance has changed)
 		return gotoAccount(request);
 	}
 
@@ -228,13 +229,12 @@ public class Controller extends HttpServlet
 		try
 		{
 			withdrawAmount = validatePositiveDouble(inputAmount);
-		}
-		catch(MBankException e)
+		} catch (MBankException e)
 		{
 			String error = e.getLocalizedMessage();
 			request.setAttribute(WITHDRAW_ERROR_ATTR, error);
 		}
-		if(request.getAttribute(WITHDRAW_ERROR_ATTR) == null)
+		if (request.getAttribute(WITHDRAW_ERROR_ATTR) == null)
 		{
 			try
 			{
@@ -245,61 +245,62 @@ public class Controller extends HttpServlet
 				request.setAttribute(WITHDRAW_ERROR_ATTR, e.getLocalizedMessage());
 				e.printStackTrace();
 			}
-				
+
 		}
 		setCommissionRateInRequest(request, clientAction);
-		
-		//refresh the account.jsp page (account balance has changed)
+
+		// refresh the account.jsp page (account balance has changed)
 		return gotoAccount(request);
 	}
 
 	private double validatePositiveDouble(String amount) throws MBankException
 	{
 		double amountValue = 0;
-		if(amount.trim().isEmpty())
+		if (amount.trim().isEmpty())
 		{
 			throw new MBankException("Amount cannot be empty");
 		}
 		try
 		{
 			amountValue = Double.parseDouble(amount);
-		}
-		catch(NumberFormatException e)
+		} catch (NumberFormatException e)
 		{
 			throw new MBankException("Amount must be a number.\n");
 		}
-		if(amountValue < 0)
+		if (amountValue < 0)
 		{
 			throw new MBankException("Amount must be non-negative\n");
 		}
 		return amountValue;
 	}
 
-	private String logout(HttpServletRequest request) {
+	private String logout(HttpServletRequest request)
+	{
 		request.getSession().invalidate();
 		return INDEX_JSP;
 	}
 
-	private String login(HttpServletRequest request) throws MBankException 
+	private String login(HttpServletRequest request) throws MBankException
 	{
-		// When logging in it is expected that no session exists (the index.jsp is forwarded to the account
+		// When logging in it is expected that no session exists (the index.jsp
+		// is forwarded to the account
 		// page is a session already exists
-		
+
 		// Create a new session
 		request.getSession(true);
-		
+
 		Action clientAction = null;
 		String username = request.getParameter(USERNAME_PARAM);
 		String password = request.getParameter(PASSWORD_PARAM);
 		String error = null;
-		//TODO validations on username/password
+		// TODO validations on username/password
 		request.getSession().setAttribute(USERNAME_ATTR, username);
 		// attempt to perform login with the provided credentials
-		try 
+		try
 		{
 			MBank mbank = (MBank) this.getServletContext().getAttribute(MBABK_INSTANCE_ATTR);
 			clientAction = mbank.login(username, password);
-		} catch (MBankException e) 
+		} catch (MBankException e)
 		{
 			// login failed
 			error = e.getLocalizedMessage();
@@ -307,8 +308,8 @@ public class Controller extends HttpServlet
 			request.getSession().invalidate();
 			return INDEX_JSP;
 		}
-		
-		if(clientAction != null)
+
+		if (clientAction != null)
 		{
 			// save the ClientAction object in the session
 			request.getSession().setAttribute(CLIENT_ACTION_ATTR, clientAction);
@@ -317,8 +318,7 @@ public class Controller extends HttpServlet
 			System.out.println("going to: " + ACCOUNT_JSP);
 			setCommissionRateInRequest(request, clientAction);
 			return gotoAccount(request);
-		}
-		else
+		} else
 		{
 			request.getSession().invalidate();
 		}
@@ -338,10 +338,10 @@ public class Controller extends HttpServlet
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 	}
 
-	private String gotoMBankProperties(HttpServletRequest request) 
+	private String gotoMBankProperties(HttpServletRequest request)
 	{
 		ClientAction clientAction = (ClientAction) request.getSession().getAttribute(CLIENT_ACTION_ATTR);
 		List<Property> system_properties = null;
@@ -354,19 +354,19 @@ public class Controller extends HttpServlet
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return MBANK_PROPERTIES_JSP;	//next page
-		
+		return MBANK_PROPERTIES_JSP; // next page
+
 	}
 
-	private String gotoMyDetails(HttpServletRequest request) throws MBankException 
+	private String gotoMyDetails(HttpServletRequest request) throws MBankException
 	{
 		ClientAction clientAction = (ClientAction) request.getSession().getAttribute(CLIENT_ACTION_ATTR);
 		Client client = clientAction.viewClientDetails();
 		request.getSession().setAttribute(CLIENT_ATTR, client);
-		return MY_DETAILS_JSP;	//next page
+		return MY_DETAILS_JSP; // next page
 	}
 
-	private String gotoDeposits(HttpServletRequest request) 
+	private String gotoDeposits(HttpServletRequest request)
 	{
 		ClientAction clientAction = (ClientAction) request.getSession().getAttribute(CLIENT_ACTION_ATTR);
 		List<Deposit> deposits = null;
@@ -380,10 +380,10 @@ public class Controller extends HttpServlet
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return DEPOSITS_JSP;	//next page
+		return DEPOSITS_JSP; // next page
 	}
 
-	private String gotoRecentActivities(HttpServletRequest request) 
+	private String gotoRecentActivities(HttpServletRequest request)
 	{
 		ClientAction clientAction = (ClientAction) request.getSession().getAttribute(CLIENT_ACTION_ATTR);
 		try
@@ -394,10 +394,10 @@ public class Controller extends HttpServlet
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return RECENT_ACTIVITIES_JSP;	//next page
+		return RECENT_ACTIVITIES_JSP; // next page
 	}
 
-	private String gotoAccount(HttpServletRequest request) throws MBankException 
+	private String gotoAccount(HttpServletRequest request) throws MBankException
 	{
 		System.out.println("Controller.gotoAccount()");
 		// Get ClientAction object from the session
@@ -406,6 +406,6 @@ public class Controller extends HttpServlet
 		System.out.println("account_id = " + account.getAccount_id());
 		request.getSession().setAttribute(ACCOUNT_ATTR, account);
 		setCommissionRateInRequest(request, clientAction);
-		return ACCOUNT_JSP;	//next page
+		return ACCOUNT_JSP; // next page
 	}
 }
