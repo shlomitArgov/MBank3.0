b3242ec99cbdd2f124ebb8b8bb8ee2a5798172df
shlomitArgov <shlomit.argov@gmail.com>
2014-11-09 10:52:29 +0200

    Fixed loginPageFilter: added check for valid clientAction within the session.

--- MBankWeb/src/mbank/LoginPageFilter.java
+++ MBankWeb/src/mbank/LoginPageFilter.java
@@ -10,18 +10,42 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet Filter implementation class LoginPageFilter
- */
 @WebFilter("/index.jsp")
 public class LoginPageFilter implements Filter 
 {
     private static final String ACCOUNT_JSP = "/account.jsp";
-	private static final String APPLICATION_NAME = "/MBankWeb"; 
+	private static final String APPLICATION_NAME = "/MBankWeb";
+	private static final String CLIENT_ACTION_ATTR = "client_action";
+	private static final String FORWARD_PATH = "/Controller?command=account"; 
 
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	// When valid session and clientAction objects already exist, forward requests to the login page to the default landing page (ACCOUNT_JSP) 
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
+	{
+		// TODO remove trace message
+		System.out.println("LoginPageFilter.doFilter()");
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession(false);
+
+		// If a valid session already exists and reached this filter (url is index.jsp) and the session context already contains a valid clientAction attribute - 
+		// forward to the default landing page (ACCOUNT_JSP)
+		if(session != null && req.getSession().getAttribute(CLIENT_ACTION_ATTR) != null)
+		{
+			// TODO remove trace message
+			System.out.println("Valid session exists - forwarding to " + APPLICATION_NAME + ACCOUNT_JSP);
+			req.getRequestDispatcher(FORWARD_PATH).forward(request, response);
+		}
+		else
+		{
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}
+	}
+	
 	/**
      * Default constructor. 
      */
@@ -38,35 +62,8 @@ public class LoginPageFilter implements Filter
 	}
 
 	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
-	{
-		System.out.println("LoginPageFilter.doFilter()");
-		// TODO Auto-generated method stub
-		// place your code here
-		HttpServletRequest req = (HttpServletRequest)request;
-		HttpSession session = req.getSession(false);
-
-		// If a valid session already exists and reached this filter (url is index.jsp)
-		if(session != null)
-		{
-			System.out.println("Valid session exists - forwarding to " + APPLICATION_NAME + ACCOUNT_JSP);
-			req.getRequestDispatcher("/Controller?command=account").forward(request, response);
-		}
-		else
-		{
-			// pass the request along the filter chain
-			chain.doFilter(request, response);
-		}
-		
-	}
-
-	/**
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
 	}
-
 }
