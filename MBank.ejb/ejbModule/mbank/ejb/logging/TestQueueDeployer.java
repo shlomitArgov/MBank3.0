6ae2efae3b965baef65ef893d8810e507688acd1
shlomitArgov <shlomit.argov@gmail.com>
2014-11-27 18:32:18 +0200

    Added QueueDeployer (to create a queue to which log message are sent).

---
+++ MBank.ejb/ejbModule/mbank/ejb/logging/TestQueueDeployer.java
@@ -0,0 +1,20 @@
+package mbank.ejb.logging;
+
+
+import javax.ejb.Stateless;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSDestinationDefinitions;
+
+@Stateless
+@JMSDestinationDefinitions(
+		@JMSDestinationDefinition(
+				name="java:/jms/queue/test",
+				interfaceName="javax.jms.Queue",
+				destinationName="testQueue",
+				description="This is a test queue for deployment"
+				
+		)
+)
+public class TestQueueDeployer {
+
+}
