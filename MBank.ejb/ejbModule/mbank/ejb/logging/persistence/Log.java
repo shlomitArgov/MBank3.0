fb99abfd5cdb294ca46d4536c630d4b8723e9999
shlomitArgov <shlomit.argov@gmail.com>
2014-11-27 20:19:27 +0200

    Log Entity bean will be persisted to the H2 DB on WildFly. LogDao - local session bean will be used to persist the Log entity bean

---
+++ MBank.ejb/ejbModule/mbank/ejb/logging/persistence/Log.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package mbank.ejb.logging.persistence;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.ejb.Local;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+/**
+ * @author Shlomit Argov
+ *
+ */
+@Entity
+public class Log implements Serializable
+{
+	@Override
+	public String toString()
+	{
+		return "Log [id=" + id + ", clientId=" + clientId + ", operation=" + operation + ", timestamp=" + timestamp + "]";
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@GeneratedValue
+	private Long id;
+	private Long clientId;
+	private String operation;
+	private Date timestamp;
+	
+	public Log(){}; // Default constructor 
+	
+	public Log(Long clientId, String operation)
+	{
+		this.clientId = clientId;
+		this.operation = operation;
+		this.timestamp = new Date();
+	}
+	
+	public Long getId()
+	{
+		return id;
+	}
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+	public Long getClientId()
+	{
+		return clientId;
+	}
+	
+	public String getOperation()
+	{
+		return operation;
+	}
+}
---
+++ MBank.ejb/ejbModule/mbank/ejb/logging/persistence/LogDAO.java
@@ -0,0 +1,20 @@
+package mbank.ejb.logging.persistence;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class LogDAO
+ */
+@Stateless
+@LocalBean
+public class LogDAO {
+
+    /**
+     * Default constructor. 
+     */
+    public LogDAO() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
